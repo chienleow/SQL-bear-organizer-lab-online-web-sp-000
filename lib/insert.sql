21a574251be43490ad58ecf8aaaa49daf48647ee
C. Leow <chienleow1@gmail.com>
2019-07-13 20:40:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown','15','male');
 INSERT INTO bears (name, age, gender) VALUES ('Melissa','10','female');
 INSERT INTO bears (name, age, gender) VALUES ('Grinch','18','male');
 INSERT INTO bears (name, age, gender) VALUES ('Wendy','12','female');
-INSERT INTO bears (name, age, gender) VALUES (NULL);
+INSERT INTO bears (name, age, gender) VALUES (NULL,'1','female');
