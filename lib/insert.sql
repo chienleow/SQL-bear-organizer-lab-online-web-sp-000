f9481e754897b8c3f7f9f246f36176a046c2455f
C. Leow <chienleow1@gmail.com>
2019-07-13 21:04:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender) VALUES ('Mr. Chocolate','6','male');
 INSERT INTO bears (name, age, gender) VALUES ('Rowdy','9','male');
-INSERT INTO bears (name, age, gender) VALUES ('Tabitha','3', 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Tabitha','3','female');
 INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown','15','male');
 INSERT INTO bears (name, age, gender) VALUES ('Melissa','10','female');
 INSERT INTO bears (name, age, gender) VALUES ('Grinch','18','male');
