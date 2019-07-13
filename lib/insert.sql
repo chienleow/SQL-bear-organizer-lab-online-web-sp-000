6b6f65d8070179f816959f3430cab4886e2ec37a
C. Leow <chienleow1@gmail.com>
2019-07-13 20:08:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name) VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha', 'Sergeant Brown', '');
+INSERT INTO bears (name) VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha', 'Sergeant Brown', 'Melissa', '');
